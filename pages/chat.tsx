e6ff2f429383f2a150b78480a158f1dbd85c03e9
Pedro Almeida <59484474+PedroMarianoAlmeida@users.noreply.github.com>
2021-06-27 10:10:52 -0300

    Aula 10

--- pages/chat.tsx
+++ pages/chat.tsx
@@ -5,11 +5,11 @@ import Button from '@material-ui/core/Button';
 
 import { useAuthState } from 'react-firebase-hooks/auth';
 import { auth } from './../src/config/firebaseConfig';
+import UsuárioCarregado from '../src/components/domain/chat/UsuárioCarregado';
 
 const Chat = () => {
   const router = useRouter();
   const [user, loading, error] = useAuthState(auth);
-  console.log(user);
 
   useEffect(() => {
     if (!loading && !user) {
@@ -27,6 +27,8 @@ const Chat = () => {
       >
         Logout
       </Button>
+
+      {user && <UsuárioCarregado user={user} />}
     </>
   );
 };
